1007f6e9ec3f0fe61eb73c19893f0748046f4fe4
Emily Asadoorian <epa5328@psu.edu>
2022-06-06 09:54:33 -0400

    Init with Spring Initializr

---
+++ src/main/java/edu/psu/sweng/pawportal/PawPortalApplication.java
@@ -0,0 +1,13 @@
+package edu.psu.sweng.pawportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/psu/sweng/pawportal/PawPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.psu.sweng.pawportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
