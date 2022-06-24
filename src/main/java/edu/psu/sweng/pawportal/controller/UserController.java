2797276df85aea9de01dcb6f20bab4cd1db89894
Kevin Muirhead <kxm5887@psu.edu>
2022-06-24 11:47:55 -0400

    Working on Controllers and RequestMapping. Can't get the it to map correctly to the HTML file

--- src/main/java/edu/psu/sweng/pawportal/controller/UserController.java
+++ src/main/java/edu/psu/sweng/pawportal/controller/UserController.java
@@ -1,8 +1,16 @@
 package edu.psu.sweng.pawportal.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class UserController {
+
+    @RequestMapping (value = "/homepage", method = RequestMethod.POST)
+    @ResponseBody
+    public String homePage(){
+        return "homepage";
+    }
 }
