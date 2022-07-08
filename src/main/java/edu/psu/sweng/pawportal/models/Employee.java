cfa4e3c886ba035b5428a8f024546fbabe87128b
Kien Nguyen <kqn5256@psu.edu>
2022-07-08 09:03:33 -0400

    Updated Employee and added EmployeeRepository

--- src/main/java/edu/psu/sweng/pawportal/models/Employee.java
+++ src/main/java/edu/psu/sweng/pawportal/models/Employee.java
@@ -1,15 +1,102 @@
 package edu.psu.sweng.pawportal.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
-public class Employee extends Person{
+public class Employee{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    public boolean isManager;
+    private boolean isManager;
+
+    @Column(nullable = false, length = 64)
+    private String firstName;
+    @Column(nullable = false, length = 64)
+    private String lastName;
+    @Column(nullable = false, length = 64)
+    private String phoneNum;
+    @Column(nullable = false, unique = true, length = 64)
+    private String email;
+    @Column(nullable = false, length = 64)
+    private String address;
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String phoneNum, String email, String address, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNum = phoneNum;
+        this.email = email;
+        this.address = address;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean getIsManager() {
+        return isManager;
+    }
+
+    public void setIsManager(Boolean manager) {
+        this.isManager = manager;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
