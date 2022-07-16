f363829f5c556fa90a42f712c01b0e28c4239bbd
Kevin Muirhead <kxm5887@psu.edu>
2022-07-16 09:09:57 -0400

    WebSec for login authentication

---
+++ src/main/java/edu/psu/sweng/pawportal/security/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package edu.psu.sweng.pawportal.security;
+
+import edu.psu.sweng.pawportal.models.Customer;
+import edu.psu.sweng.pawportal.models.CustomerUserDetails;
+import edu.psu.sweng.pawportal.services.CustomerUserDetailsServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig{
+
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new CustomerUserDetailsServices();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/user_{id}").authenticated()
+                .anyRequest().permitAll()
+                .and().formLogin()
+                .usernameParameter("email")
+                .defaultSuccessUrl("/user_{id}")
+                .permitAll()
+                .and()
+                .logout().logoutSuccessUrl("/homepage").permitAll();
+        return http.build();
+    }
+
+}
