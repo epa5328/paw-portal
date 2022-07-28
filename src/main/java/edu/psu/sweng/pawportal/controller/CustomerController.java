ddd8e1ec6905abe7bff88441bac0092ec11b86e0
Emily Asadoorian <epa5328@psu.edu>
2022-07-28 19:37:37 -0400

    Pass Java objects through to Thymeleaf engine

--- src/main/java/edu/psu/sweng/pawportal/controller/CustomerController.java
+++ src/main/java/edu/psu/sweng/pawportal/controller/CustomerController.java
@@ -1,12 +1,23 @@
 package edu.psu.sweng.pawportal.controller;
 
+import java.util.HashSet;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import edu.psu.sweng.pawportal.models.*;
 
 @Controller
 public class CustomerController {
     @RequestMapping("/customerMainPage")
     public String customerMainPage () {
         return "customerMainPage";
+    }
+
+    @RequestMapping("/account/{userID}")
+    public String customerAccount (Model model) {
+        Customer tempCust = new Customer("Joe", "Smith", "555-555-1212", "personal@email.com", "123 Main Street", "passw0rd1", new HashSet<Dog>());
+        model.addAttribute("customer", tempCust);
+        return "accounttemp";
     }
 }
