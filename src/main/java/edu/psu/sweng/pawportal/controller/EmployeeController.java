bd61487e090fbfd3b12cb52a33d3c84ee6d8a518
Emily Asadoorian <epa5328@psu.edu>
2022-08-01 22:16:07 -0400

    Copy pseudocode to employee controller

--- src/main/java/edu/psu/sweng/pawportal/controller/EmployeeController.java
+++ src/main/java/edu/psu/sweng/pawportal/controller/EmployeeController.java
@@ -1,12 +1,51 @@
 package edu.psu.sweng.pawportal.controller;
 
+import edu.psu.sweng.pawportal.models.Employee;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import edu.psu.sweng.pawportal.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
+    /*
     @RequestMapping("/employeeMainPage")
     public String employeeMainPage () {
         return "employeeMainPage";
+    }
+    */
+
+    @Autowired
+    public EmployeeRepository repo;
+
+    // show employee
+    @GetMapping("/walker/{empID}")
+    public String showEmployee(Employee employee) {
+        // SQL query: grab employee info given ID
+        return "employeeInfoPage";
+    }
+
+    // create employee
+    @GetMapping("/walker/new")
+    public String createEmployee(Employee employee) {
+        // no employee attributes to inject
+        return "newEmployeePage";
+    }
+    
+    // edit employee
+    @GetMapping("/walker/{empID}/edit")
+    public String editEmployee(Employee employee) {
+        // inject specified employee into HTML
+        return "editEmployeePage";
+    }
+
+    // update employee (new/edit)
+    @PostMapping("/walker/{empID}")
+    public String updateCustomer(Employee employee) {
+        // take parameters and save employee
+        repo.save(employee);
+        // SQL query: grab employee info given ID
+        return "employeeInfoPage";
     }
 }
