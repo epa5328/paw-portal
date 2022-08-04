eab96c6e7c32928f0f8472460a9d9eb0573d425c
Emily Asadoorian <epa5328@psu.edu>
2022-08-04 15:33:42 -0400

    Clean up a warning

--- src/main/java/edu/psu/sweng/pawportal/repository/CustomerRepository.java
+++ src/main/java/edu/psu/sweng/pawportal/repository/CustomerRepository.java
@@ -4,8 +4,6 @@ import edu.psu.sweng.pawportal.models.Customer;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
     //This helps with repository search when a user is logging in
     @Query("SELECT u FROM Customer u WHERE u.email = ?1")
