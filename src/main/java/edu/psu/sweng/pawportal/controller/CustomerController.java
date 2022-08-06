45443caaf6f5bacfb73430a4f6d2228b5a057375
Emily Asadoorian <epa5328@psu.edu>
2022-08-06 13:25:11 -0400

    Supply customer object to account view

--- src/main/java/edu/psu/sweng/pawportal/controller/CustomerController.java
+++ src/main/java/edu/psu/sweng/pawportal/controller/CustomerController.java
@@ -1,38 +1,24 @@
 package edu.psu.sweng.pawportal.controller;
 
-//import edu.psu.sweng.pawportal.models.Dog;
+import org.springframework.ui.Model;
 import edu.psu.sweng.pawportal.models.Customer;
 import org.springframework.stereotype.Controller;
 import edu.psu.sweng.pawportal.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
-    /*
-    @RequestMapping("/customerMainPage")
-    public String customerMainPage () {
-        return "customerMainPage";
-    }
-
-    @RequestMapping("/account/{id}")
-    public String customerAccount (Model model, @PathVariable Long id) {
-        Customer tempCust = new Customer("Joe", "Smith", "555-555-1212", "personal@email.com", "123 Main Street", "passw0rd1", new HashSet<Dog>());
-        model.addAttribute("customer", tempCust);
-        //model.addAttribute("customer", Customer.getCustomer(id));
-        return "accounttemp";
-    }
-    */
-
     @Autowired
     public CustomerRepository repo;
 
     // show customer
     @GetMapping("/account/{userID}")
-    public String showCustomer(Customer customer) {
-        // SQL query: grab customer info given ID
-        return "customerInfoPage";
+    public String showCustomer(Model model, @PathVariable("userID") long userID) {
+        model.addAttribute("customer", repo.findById(userID));
+        return "accounttemp";
     }
 
     // create customer
